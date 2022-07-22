1a681848c58dd05d0e6b6acda74a70cedfeb0c90
AleDella <45104686+AleDella@users.noreply.github.com>
2022-07-22 18:22:05 +0200

    Add sentence2graph test snippet code for reference

---
+++ produce_graph_libs.py
@@ -0,0 +1,46 @@
+import sng_parser
+import dgl
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+# Function that takes a sentence and produce the dgl graph of it
+def sentence_to_dgl(sentence, visualize=True):
+    # Create the graph of the sentence
+    graph = sng_parser.parse(sentence)
+    
+    
+    # Create the networkx graph to visualize it
+    G = nx.Graph()
+    for rel in graph['relations']:
+        # Add the nodes (Just two because in each relation there is only one object and one subject)
+        G.add_node(graph['entities'][rel['subject']]['head'])
+        G.add_node(graph['entities'][rel['object']]['head'])
+        # Add the relation between the two
+        G.add_edge(graph['entities'][rel['subject']]['head'],graph['entities'][rel['object']]['head'], relation=rel['relation'])
+        if visualize:
+            pos = nx.spring_layout(G)
+            # Add the edge label
+            nx.draw_networkx_edge_labels(
+                G, pos,
+                edge_labels={(graph['entities'][rel['subject']]['head'], graph['entities'][rel['object']]['head']): rel['relation'] },
+                font_color='red'
+                )
+    if visualize:
+        nx.draw(G, with_labels= True)
+        plt.show()
+    # Return the dgl network
+    return dgl.from_networkx(G)
+
+############# TEST CODE #######################
+
+# Define the sentence
+sentence = 'James is running in the park.'
+
+# Check the table to see the resulting graph for the sentence
+print("Right relations: ")
+# Parse the sentence
+graph = sng_parser.parse(sentence)
+sng_parser.tprint(graph)
+
+result = sentence_to_dgl(sentence, visualize=True)
