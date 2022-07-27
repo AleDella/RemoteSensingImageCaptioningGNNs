378afdf0c14c1d58ac0352488f98f5e276147106
RickyRicci <riccardo.ricci-1@unitn.it>
2022-07-27 14:34:06 +0200

    Extracted triplet from UCM dataset to train classifier

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 For now, there are two files in the repo:
     <li> <code>graph_utils.py</code> that contains the final function that will be used to pre-process the captions</li>
-    <li> <code>prodcue_graph_libs.py</code> that is used as demo to understand graphs from senteces</li>
+    <li> <code>produce_graph_libs.py</code> that is used as demo to understand graphs from senteces</li>
--- extract_triplets.py
+++ extract_triplets.py
@@ -2,51 +2,125 @@ import json
 import sng_parser
 
 def extract_ent(sentences):
-    '''
-    Extract the features for the relevant words in the caption. 
-    NB: "relevant" means that are the labels of nodes and edges of the scene graphs
-
-    Input:
-        sentences: list of sentences
-        save_feats: if True, saves the features for the words in a json file (Default False)
-        save_model: if True, saves the word2vec model in the same folder (Default False)
-        loaded_model: if not None, load the word2vec model indicated in the specified path (Default None)
-        tokenize: if True, returns the tokenized sentences using entities of the graph
-    
-    Return:
-        model: the word2vec model
-        final_input: tokenized sentences
-    '''
-
     final_input = []
-    for sent in sentences:
-        g = sng_parser.parse(sent['raw'])
+    for sentence in sentences:
+        g = sng_parser.parse(sentence['raw'])
+        # Get the tokenization like in the graph
         for rel in g['relations']:
             final_input.append((g['entities'][rel['subject']]['head'], rel['relation'], g['entities'][rel['object']]['head']))
     return final_input
 
+def extract_triplets(sentence):
+    # Extract triplets from one sentence
+    final_input = []
+    g = sng_parser.parse(sentence)
+    # Get the tokenization like in the graph
+    for rel in g['relations']:
+        final_input.append((g['entities'][rel['subject']]['head'], rel['relation'], g['entities'][rel['object']]['head']))
+    return final_input
+
+# FOR RSICD
+
+# anno_path = "Provone/dataset/dataset_rsicd.json"
+# f = open(anno_path)
+# anno = json.load(f)
+# f.close()
+# anno = anno['images']
+# final_file = {'annotations': []}
+# final_labels = []
+
+# for img in anno:
+#     tripl = list(set(extract_ent(img['sentences'])))
+#     if tripl not in final_labels:
+#         final_labels.append(tripl)
+#     x = {'imgid': img['imgid'], 'triplets': tripl}
+#     final_file['annotations'].append(x)
+
+# with open("Provone/triplets.json", "w") as outfile:
+#     json.dump(final_file, outfile)
+
+# print("Total number of unique triplets found: ", len(final_labels))
+def readfile(path):
+    with open(path,'r') as file:
+        text = file.readlines()
+    return text
 
 
+# FOR UCM
+path_annots = "D:/Alessio/Provone/dataset/UCM_dataset/filenames/descriptions_UCM.txt"
+train = "D:/Alessio/Provone/dataset/UCM_dataset/filenames/filenames_train.txt"
+test  = "D:/Alessio/Provone/dataset/UCM_dataset/filenames/filenames_test.txt"
+val = "D:/Alessio/Provone/dataset/UCM_dataset/filenames/filenames_val.txt"
 
+vocabulary = dict()
 
+annotations = readfile(path_annots)
+train = readfile(train)
+test = readfile(test)
+val = readfile(val)
 
-anno_path = "dataset/dataset_rsicd.json"
-f = open(anno_path)
-anno = json.load(f)
-f.close()
-anno = anno['images']
-final_file = {'attributes': []}
-number = 0
-for img in anno:
-    tripl = list(set(extract_ent(img['sentences'])))
-    img_dict = {'filename': img['filename'], 'imgid': img['imgid'], 'triplets': tripl}
-    final_file['attributes'].append(img_dict)
-    number+=len(tripl)
+train_list = []
+test_list = []
+val_list = []
+
+for img in train:
+    train_list.append(img.split(".")[0])
+
+for img in test:
+    test_list.append(img.split(".")[0])
+
+for img in val:
+    val_list.append(img.split(".")[0])
+
+# Create vocabulary
+for annot in annotations:
+    tokens = annot.split(" ")[1:]
+    for token in tokens:
+        try:
+            vocabulary[token]+=1
+        except:
+            vocabulary[token]=1
+
+# Filter words with occurrence less than 5 
+words_to_keep = [key for key in vocabulary.keys() if vocabulary[key]>5]
+words_to_discard = [key for key in vocabulary.keys() if key not in words_to_keep]
+
+# Create the triplets and store them in the dictionary
+final_file = {'train': {}, 'test': {}, 'val': {}}
+all_triplets = []
+for annot in annotations:
+    print(annot)
+    image = annot.split(" ")[0]
+    triplets = extract_triplets(annot)
+    print(triplets)
+    if(image in train_list):
+        final_file["train"][image] = triplets
+    elif(image in test_list):
+        final_file["test"][image] = triplets
+    elif(image in val_list):
+        final_file["val"][image] = triplets
+    else:
+        raise Exception("Something went wrong, find an image not assigned to any of train test or val splits")     
     
-
+    for triplet in triplets:
+        all_triplets.append(triplet)
+    
 with open("triplets.json", "w") as outfile:
     json.dump(final_file, outfile)
+    
+all_triplets = set(all_triplets)
 
-print("Total number of triplets found: ", number)
+print("Total number of unclean triplets found: ", len(all_triplets))
 
+# Cleaning triplets
+clean_triplets = []
+for triplet in all_triplets:
+    discard = False
+    for word in triplet:
+        if word in words_to_discard:
+            discard = True
+            break
+    if(not discard):
+        clean_triplets.append(triplet)
 
+print("Total number of cleaned triplets found: ", len(clean_triplets))
