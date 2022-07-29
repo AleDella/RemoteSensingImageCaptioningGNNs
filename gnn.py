c2da82f98933a70cc4e1022d993308cbf9cef8be
AleDella <alessiodellasega@gmail.com>
2022-07-29 17:28:42 +0200

    Added Ito-san's LSTMDecoder

--- gnn.py
+++ gnn.py
@@ -55,6 +55,53 @@ class GNN(nn.Module):
         return hg
 
 
+##################################
+# Taken from MLAP code
+def _encode_seq_to_arr(seq: list[str], vocab2idx: dict[str, int], max_seq_len: int) -> torch.Tensor:
+    seq = seq[:max_seq_len] + ["__EOS__"] * max(0, max_seq_len - len(seq))
+    return torch.tensor([vocab2idx[w] if w in vocab2idx else vocab2idx["__UNK__"] for w in seq], dtype=torch.int64)
+
+class LSTMDecoder(nn.Module):
+    def __init__(self, dim_feat: int, max_seq_len: int, vocab2idx: dict[str, int]):
+        super().__init__()
+        self._max_seq_len = max_seq_len
+        self._vocab2idx = vocab2idx
+
+        self.lstm = nn.LSTMCell(dim_feat, dim_feat)
+        self.w_hc = nn.Linear(dim_feat * 2, dim_feat)
+        self.layernorm = nn.LayerNorm(dim_feat)
+        self.vocab_encoder = nn.Embedding(len(vocab2idx), dim_feat)
+        self.vocab_bias = nn.Parameter(torch.zeros(len(vocab2idx)))
+
+    def forward(self, graph: dgl.DGLGraph, feats: torch.Tensor, labels: any):
+        if self.training:
+            # teacher forcing
+            batched_label = torch.vstack([_encode_seq_to_arr(label, self._vocab2idx, self._max_seq_len - 1) for label in labels])
+            batched_label = torch.hstack((torch.zeros((graph.batch_size, 1), dtype=torch.int64), batched_label))
+            true_emb = self.vocab_encoder(batched_label.to(device=graph.device))
+
+        h_t, c_t = feats[-1].clone(), feats[-1].clone()
+        feats = feats.transpose(0, 1)  # (batch_size, L + 1, dim_feat)
+        out = []
+        pred_emb = self.vocab_encoder(torch.zeros((graph.batch_size), dtype=torch.int64, device=graph.device))
+
+        vocab_mat = self.vocab_encoder(torch.arange(len(self._vocab2idx), dtype=torch.int64, device=graph.device))
+
+        for i in range(self._max_seq_len):
+            if self.training:
+                _in = true_emb[:, i]
+            else:
+                _in = pred_emb
+            h_t, c_t = self.lstm(_in, (h_t, c_t))
+
+            a = F.softmax(torch.bmm(feats, h_t.unsqueeze(-1)).squeeze(-1), dim=1)  # (batch_size, L + 1)
+            context = torch.bmm(a.unsqueeze(1), feats).squeeze(1)
+            pred_emb = torch.tanh(self.layernorm(self.w_hc(torch.hstack((h_t, context)))))  # (batch_size, dim_feat)
+
+            out.append(torch.matmul(pred_emb, vocab_mat.T) + self.vocab_bias.unsqueeze(0))
+
+        return out
+###########################
 
 # Testing
 if __name__ == '__main__':
